aac53c9624d3b92800b89396cfb9912d24edf5e7
Kadavi Savage <coedry@gmail.com>
2012-08-28 11:36:13 -0700

    *

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+public class Fibonacci {
+
+    public static int fib(int num) {
+        
+        if (num <= 2)
+            return 1;
+        else
+            return fib(num-1) + fib(num-2);
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        System.out.println(fib(6)); //8
+        
+    }
+}
