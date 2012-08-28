bbc234c5e4b94bbd4d1bccfd08ce9e8881fa6d05
Kadavi <coedry@gmail.com>
2012-08-28 11:43:20 -0700

    Update Fibonacci.java
    Added comments

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,11 +1,11 @@
 public class Fibonacci {
 
     public static int fib(int num) {
-        
+        // Exit at 2 because anything before it equals 1
         if (num <= 2)
             return 1;
         else
-            return fib(num-1) + fib(num-2);
+            return fib(num-1) + fib(num-2); // Avoiding fib(num) because it will infinite loop
         
     }
     
