86d61d0683fc6db0cc7d5af97d3b77d74f531fcc
Grant Dawson <coedry@gmail.com>
2012-10-24 23:25:41 -0700

    Update Fibonacci.java
    Was one off.

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,7 +2,7 @@ public class Fibonacci {
 
     public static int fib(int num) {
         // Exit at 2 because anything before it equals 1
-        if (num <= 2)
+        if (num < 2)
             return 1;
         else
             return fib(num-1) + fib(num-2); // Avoiding fib(num) because it will infinite loop
@@ -11,7 +11,7 @@ public class Fibonacci {
     
     public static void main(String[] args) {
         
-        System.out.println(fib(6)); //8
+        System.out.println(fib(5)); //8
         
     }
 }
